c45fdd8ecfc6ecee2e188bbcd5aa5f92f5ba2cd9
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-09-06 12:01:34 +0300

    task done

--- string.c
+++ string.c
@@ -1,102 +1,212 @@
 #include "main.h"
-/**
- * _strcmpdir - compares strings to find dir.
- *
- * @s1: string.
- * @s2: string.
- *
- * Return: if match and any other number if otherwise.
- **/
-int _strcmpdir(char *s1, char *s2)
-{
-	int strc = 0;
 
-	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
+/**
+ * _strcpy - makes a copy of a string
+ * @dest: address of buffer to copy string to
+ * @src: address of string to be copied
+ *
+ * Return: the pointer to dest. Otherwise NULL
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	unsigned int scpy;
+
+	if (src == NULL || dest == NULL)
+		return (NULL);
+
+	for (scpy = 0; src[scpy] != '\0'; scpy++)
+		dest[scpy] = src[scpy];
+
+	dest[scpy] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - finds the length of a given string
+ * @str: given string
+ *
+ * Return: length of str
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (*str)
 	{
-		if (strc == 3)
+		len++;
+		str++;
+	}
+
+	return (len);
+}
+
+/**
+* _strcmp - compares 2 strings
+* @s1: first string
+* @s2: second string
+*
+* Description: works exactly like strcmp from <string.h>
+*
+* Return: -ve integer, 0 or +ve integer if s1 is less than,
+* equal to or greater than s2
+*/
+int _strcmp(const char *s1, const char *s2)
+{
+	int diff = 0;
+
+	while (diff == 0)
+	{
+		diff = *s1 - *s2;
+
+		/* break if one of the strings is empty */
+		if (*s1 == '\0' || *s2 == '\0')
 			break;
-		strc++;
+
+		s1++;
 		s2++;
 	}
 
-	return (*s1 - *s2);
+	return (diff);
 }
+
 /**
- * charput - writes the character like putchar
- * @c: The character to print
+ * _strdup - creates a copy of a given string
+ * @str: string to copy
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: new string identical to str. Otherwise NULL
  */
-int charput(char c)
+char *_strdup(const char *str)
 {
-	return (write(1, &c, 1));
-}
+	unsigned int len;
+	char *result;
 
-/**
- * place - similar to puts in C
- * @str: a pointer the integer we want to set to 402
- *
- * Return: int
- */
-void place(char *str)
-{
-	while (*str != '\0')
-	{
-		charput(*str);
-		str++;
-	}
-}
-
-/**
- * _strlen - Len string.
- * @str: My string.
- * Return: Length.
- */
-int _strlen(char *str)
-{
-	int slen;
-
-	for (slen = 0; str[slen] != '\0'; slen++)
-		;
-
-	return (slen);
-}
-
-/**
- * str_concat - concatane strings.
- * @s1: string.
- * @s2: second string.
- * Return: strings.
- */
-char *str_concat(char *s1, char *s2)
-{
-	char *a;
-	int lens1, lens2, x, w, e;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	lens1 = _strlen(s1);
-
-	lens2 = _strlen(s2);
-
-	a = malloc(((lens1) + (lens2) + 1) * sizeof(char));
-
-	if (a == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	for (x = 0; x < lens1; x++)
+	/* allocate space for new string */
+	len = _strlen(str);
+	result = malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	/* copy contents of str into new string */
+	return (_strcpy(result, str));
+}
+
+/**
+ * _strtok - tokenizes a given string using a given delimiter
+ * @str: string
+ * @delim: delimiter
+ *
+ * Return: pointer to next token. Otherwise NULL
+ */
+char *_strtok(char *str, const char *delim)
+{
+	char c;
+	int found_char = 0;
+	char *start;
+	static char *current;
+
+	start = (str ? str : current);
+
+	if (str)
+		current = str;
+
+	if (delim == NULL || *current == '\0')
+		return (NULL);
+
+	c = delim[0];
+
+	while (*current == c) /* ignore separators at beginning */
 	{
-		a[x] = s1[x];
+		start++;
+		current++;
 	}
 
-	for (w = lens1, e = 0; e <= lens2; w++, e++)
+	while (*current)
 	{
-		a[w] = s2[e];
+		if (*current == c && found_char) /* found a delimiter */
+		{
+			*current = '\0';
+			current++;
+			break;
+		}
+
+		found_char = 1;
+		current++;
 	}
-	return (a);
+
+	while (*current && *current == c) /* ignore separators at end */
+		current++;
+
+	return (_strlen(start) ? start : NULL);
+}
+
+/**
+ * is_in_str - checks whether given character is in a given string
+ * @str: string
+ * @c: character
+ *
+ * Return: 1 if c is in str. 0 Otherwise
+ */
+int is_in_str(const char *str, char c)
+{
+	if (str == NULL)
+		return (0);
+
+	while (*str)
+	{
+		if (*str == c)
+			return (1);
+
+		str++;
+	}
+
+	return (0);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @str: string
+ *
+ * Return: integer equivalent. Otherwise -1
+ */
+int _atoi(char *str)
+{
+	int x, j, n, result;
+
+	if (str == NULL)
+		return (-1);
+
+	n = _strlen(str);
+
+	for (x = n - 1, j = 1, result = 0; x >= 0; x--, j *= 10)
+	{
+		if (result < 0 || str[x] > '9' || str[x] < '0')
+			return (-1);
+
+		result += (str[x] - '0') * j;
+	}
+
+	return (result % 256);
+}
+
+/**
+ * str_rep - replaces all occurences of a non printable character
+ * in a string with space
+ * @str: string
+ */
+void str_rep(char *str)
+{
+	while (*str)
+	{
+		if (*str < ' ')
+			*str = ' ';
+
+		str++;
+	}
 }
