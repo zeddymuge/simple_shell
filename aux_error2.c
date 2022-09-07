b78733f05f41802f31e9f6e70fe908e7754c9891
Mkiige <106949498+Mkiige@users.noreply.github.com>
2022-09-07 13:32:39 +0300

    Task Done

--- aux_error2.c
+++ aux_error2.c
@@ -7,15 +7,15 @@
  */
 char *error_env(data_shell *datash)
 {
-	int length;
+	int len;
 	char *error;
 	char *ver_str;
 	char *msg;
 
 	ver_str = aux_itoa(datash->counter);
 	msg = ": Unable to add/remove from environment\n";
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + _strlen(msg) + 4;
+	len = _strlen(datash->av[0]) + _strlen(ver_str);
+	len += _strlen(datash->args[0]) + _strlen(msg) + 4;
 	error = malloc(sizeof(char) * (length + 1));
 	if (error == 0)
 	{
@@ -43,13 +43,13 @@ char *error_env(data_shell *datash)
  */
 char *error_path_126(data_shell *datash)
 {
-	int length;
+	int len;
 	char *ver_str;
 	char *error;
 
 	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + 24;
+	len = _strlen(datash->av[0]) + _strlen(ver_str);
+	len += _strlen(datash->args[0]) + 24;
 	error = malloc(sizeof(char) * (length + 1));
 	if (error == 0)
 	{
